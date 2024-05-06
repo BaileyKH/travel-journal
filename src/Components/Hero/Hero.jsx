2042fc046cf4f9f4d0bb6873fc6446a1f589e56c
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-05-06 11:32:45 -0700

    hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,32 @@
 import './Hero.css'
 
+import { travels } from '/src/TravelData.js'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
 export const Hero = () => {
     return(
-        <div></div>
+        <div className="hero-container">
+            <div className="hero-inner">
+            {travels.map((travel) => (
+                <div key={travel.id}>
+                    <div className="hero-content">
+                        <img src={travel.img} className="hero-img" alt={travel.title}/>
+                        <div className="hero-content-inner">
+                            <div className="location">
+                                <FontAwesomeIcon icon={faLocationDot} style={{color: "#f55a5a",}} />
+                                <p>{travel.location.toUpperCase()}</p>
+                                <a href={travel.googleMapsUrl}>View on Google Maps</a>
+                            </div>
+                            <h1>{travel.title}</h1>
+                            <p className="dates">{travel.startDate} - {travel.endDate}</p>
+                            <p>{travel.description}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            </div>
+        </div>
     );
 }
--- src/TravelData.js
+++ src/TravelData.js
@@ -1,11 +1,32 @@
 export const travels = [
     {
+        id: 1, 
         title: "New York City",
         location: "United States",
-        googleMapsUrl: "https://www.google.com/maps/place/New+York,+NY/@40.6974881,-73.979681,10z/data=!3m1!4b1!4m6!3m5!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728!16zL20vMDJfMjg2?entry=ttu",
+        googleMapsUrl: "https://shorturl.at/cDL25",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "New York City, an iconic metropolis, captivates with its dazzling skyline, cultural diversity, and relentless energy. From the historic landmarks of Manhattan to the vibrant neighborhoods of Brooklyn, each corner offers a unique slice of urban adventure.",
         img: "/src/assets/nyc.jpg"
+    },
+    {
+        id: 2,
+        title: "Rome",
+        location: "Italy",
+        googleMapsUrl: "https://shorturl.at/kmqSV",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "Rome, the eternal city, is a timeless blend of awe-inspiring history, vibrant street life, and culinary delights. Wander through the ruins of the Roman Forum, gaze up at the majestic Colosseum, and lose yourself in the narrow streets that echo with tales of emperors and artists.",
+        img: "/src/assets/rome.jpg"
+    },
+    {
+        id: 3,
+        title: "Paris",
+        location: "France",
+        googleMapsUrl: "https://shorturl.at/dCDMX",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "Paris, the city of light, enchants with its unparalleled blend of art, culture, and romance. Stroll along the Seine, explore world-renowned museums like the Louvre, and indulge in the culinary pleasures at a quaint café or a bustling brasserie.",
+        img: "/src/assets/paris.jpg"
     },
 ]
